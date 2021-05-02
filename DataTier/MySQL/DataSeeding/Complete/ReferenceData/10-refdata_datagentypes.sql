b369b36365fd6d17dbcfa9006ad41edd9a5a818a
Alan Scott <alscott@redhat.com>
2021-05-01 21:25:56 -0500

    Added updated dataseeding files and newer DDL

---
+++ DataTier/MySQL/DataSeeding/Complete/ReferenceData/10-refdata_datagentypes.sql
@@ -0,0 +1,69 @@
+INSERT INTO refdata_datagentypes (DataGenTypeID,DataGenTypeDescription,CreatedDate,StatusID,CreatedUser) VALUES
+	 ('CC_AMEX','Credit Card - AMEX','2021-05-01 18:57:33.0',1,NULL),
+	 ('CC_Discvr','Credit Card - Discover','2021-05-01 18:57:33.0',1,NULL),
+	 ('CC_MstrCrd','Credit Card - Mastercard','2021-05-01 18:57:33.0',1,NULL),
+	 ('CC_Visa','Credit Card - Visa','2021-05-01 18:55:19.0',1,NULL),
+	 ('IntlAddr','International Address','2021-05-01 17:27:55.0',1,NULL),
+	 ('LatamAddr','Latin America Address','2021-05-01 17:27:55.0',1,NULL),
+	 ('N/A','Not Applicable','2021-05-01 02:53:52.0',1,NULL),
+	 ('State_AK','Alaska','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_AL','Alabama','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_AR','Arkansas','2021-05-01 19:02:06.0',1,NULL);
+INSERT INTO refdata_datagentypes (DataGenTypeID,DataGenTypeDescription,CreatedDate,StatusID,CreatedUser) VALUES
+	 ('State_AZ','Arizona','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_CA','California','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_CO','Colorado','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_CT','Connecticut','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_DC','District of Columbia','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_DE','Delaware','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_FL','Florida','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_GA','Georgia','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_HI','Hawaii','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_IA','Iowa','2021-05-01 19:02:06.0',1,NULL);
+INSERT INTO refdata_datagentypes (DataGenTypeID,DataGenTypeDescription,CreatedDate,StatusID,CreatedUser) VALUES
+	 ('State_ID','Idaho','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_IL','Illinois','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_IN','Indiana','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_KS','Kansas','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_KY','Kentucky','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_LA','Louisiana','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_MA','Massachusetts','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_MD','Maryland','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_ME','Maine','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_MI','Michigan','2021-05-01 19:02:06.0',1,NULL);
+INSERT INTO refdata_datagentypes (DataGenTypeID,DataGenTypeDescription,CreatedDate,StatusID,CreatedUser) VALUES
+	 ('State_MN','Minnesota','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_MO','Missouri','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_MS','Mississippi','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_MT','Montana','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_NC','North Carolina','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_ND','North Dakota','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_NE','Nebraska','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_NH','New Hampshire','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_NJ','New Jersey','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_NM','New Mexico','2021-05-01 19:02:06.0',1,NULL);
+INSERT INTO refdata_datagentypes (DataGenTypeID,DataGenTypeDescription,CreatedDate,StatusID,CreatedUser) VALUES
+	 ('State_NV','Nevada','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_NY','New York','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_OH','Ohio','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_OK','Oklahoma','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_OR','Oregon','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_PA','Pennsylvania','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_PR','Puerto Rico','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_RI','Rhode Island','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_SC','South Carolina','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_SD','South Dakota','2021-05-01 19:02:06.0',1,NULL);
+INSERT INTO refdata_datagentypes (DataGenTypeID,DataGenTypeDescription,CreatedDate,StatusID,CreatedUser) VALUES
+	 ('State_TN','Tennessee','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_TX','Texas','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_UT','Utah','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_VA','Virginia','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_VT','Vermont','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_WA','Washington','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_WI','Wisconsin','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_WV','West Virginia','2021-05-01 19:02:06.0',1,NULL),
+	 ('State_WY','Wyoming','2021-05-01 19:02:06.0',1,NULL),
+	 ('UKAddr','United Kingdom Address','2021-05-01 17:27:55.0',1,NULL);
+INSERT INTO refdata_datagentypes (DataGenTypeID,DataGenTypeDescription,CreatedDate,StatusID,CreatedUser) VALUES
+	 ('USAddr','US Address','2021-05-01 17:27:55.0',1,NULL),
+	 ('USPhone','US Phone Number','2021-05-01 17:29:36.0',1,NULL);
