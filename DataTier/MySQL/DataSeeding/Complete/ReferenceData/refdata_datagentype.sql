0c5e9fe3c948d63c33c3b5307e0fbf9c50a43687
Alan Scott <alscott@redhat.com>
2021-05-17 04:03:05 -0400

    Added updated SQL including data loaders and reference datagentypes to support datagentypes enhancement to platform.

---
+++ DataTier/MySQL/DataSeeding/Complete/ReferenceData/refdata_datagentype.sql
@@ -0,0 +1,110 @@
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('1', 'Master', '^5[1-5][0-9]{14}$', 6);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('2', 'Visa', '^4[0-9]{12}(?:[0-9]{3})?$', 6);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('3', 'Discover', '^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$', 6);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('4', 'AMEX', '^3[47][0-9]{13}$', 6);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('5', 'AL', '^[0-9]{7,8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('6', 'AK', '^[0-9]{7}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('7', 'AZ', '^[A-Z]{1}[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('8', 'AR', '^[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('9', 'CA', '^[A-Z]{1}[0-9]{7}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('10', 'CO', '^[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('11', 'CT', '^[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('12', 'DE', '^[0-9]{1,7}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('13', 'DC', '^[0-9]{7}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('14', 'FL', '^[A-Z]{1}[0-9]{12}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('15', 'GA', '^[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('16', 'HI', '^H[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('17', 'ID', '^[A-Z]{3}[0-9]{6}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('18', 'IL', '^[A-Z]{1}[0-9]{11}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('19', 'IN', '^[0-9]{10}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('20', 'IA', '^[A-Z]{3}[0-9]{2}[A-Z]{4}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('21', 'KS', '^K[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('22', 'KY', '^[A-Z]{1}[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('23', 'LA', '^0[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('24', 'ME', '^[0-9]{7}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('25', 'MD', '^[A-Z]{1}[0-9]{12}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('26', 'MA', '^S[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('27', 'MI', '^[A-Z]{1}[0-9]{12}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('28', 'MN', '^[A-Z]{1}[0-9]{12}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('29', 'MS', '^[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('30', 'MO', '^[A-Z]{1}[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('31', 'MT', '^[0-9]{13}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('32', 'NE', '^[A-Z]{1}[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('33', 'NV', '^[0-9]{12}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('34', 'NH', '^[A-Z]{3}[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('35', 'NJ', '^[A-Z]{1}[0-9]{14}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('36', 'NM', '^[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('37', 'NY', '^[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('38', 'NC', '^[0-9]{12}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('39', 'ND', '^[A-Z]{3}[0-9]{6}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('40', 'OH', '^[A-Z]{2}[0-9]{6}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('41', 'OK', '^[A-Z]{1}[0-9]{9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('42', 'OR', '^[A-Z]{1}[0-9]{6,7}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('43', 'PA', '^[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('44', 'RI', '(^V[0-9]{6}$)|(^[0-9]{7}$)', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('45', 'SC', '^[0-9]{6,11}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('46', 'SD', '^[0-9]{8,9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('47', 'TN', '^[0-9]{7,9}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('48', 'TX', '^[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('49', 'UT', '^[0-9]{4,10}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('50', 'VT', '(^[0-9]{7}A$)|(^[0-9]{8}$)', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('51', 'VA', '^[A-Z]{1}[0-9]{8}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('52', 'WA', '(^[A-Z]{7}[0-9]{5}$)|(^[A-Z]{7}[0-9]{3}[A-Z]{2}$)', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('53', 'WV', '^[A-Z]{1}[0-9]{6}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('54', 'WI', '^[A-Z]{1}[0-9]{13}$', 9);
+INSERT INTO refdata_datagentypes (DataGenTypeID, DataGenTypeDescription, Definition, DataAttributeID)
+VALUES ('55', 'WY', '^[0-9]{9}$', 9);
